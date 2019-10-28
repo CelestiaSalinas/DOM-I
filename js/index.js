252eaf0fd39a8cfea67422bf2e2e1b6561160d20
Celestia Salinas <csalinas3671@gmail.com>
2019-10-28 17:42:26 -0400

    page built

--- js/index.js
+++ js/index.js
@@ -59,4 +59,64 @@ links[2].textContent = siteContent.nav["nav-item-3"];
 links[3].textContent = siteContent.nav["nav-item-4"];
 links[4].textContent = siteContent.nav["nav-item-5"];
 links[5].textContent = siteContent.nav["nav-item-6"];
-nav.style.color = "green";
+
+const ctaText = document.querySelector("h1");
+ctaText.textContent = siteContent["cta"]["h1"];
+
+const btn = document.querySelector("button");
+btn.textContent = siteContent["cta"]["button"];
+
+const topContAbout = document.querySelector(
+  ".top-content div:nth-of-type(1) h4"
+);
+topContAbout.textContent = siteContent["main-content"]["about-h4"];
+
+const para1 = document.querySelector(".top-content div:nth-of-type(1) p");
+para1.textContent = siteContent["main-content"]["about-content"];
+
+const topContFeatures = document.querySelector(
+  ".top-content div:nth-of-type(2) h4"
+);
+topContFeatures.textContent = siteContent["main-content"]["features-h4"];
+
+const para2 = document.querySelector(".top-content div:nth-of-type(2) p");
+para2.textContent = siteContent["main-content"]["features-content"];
+
+const bottomContServices = document.querySelector(
+  ".bottom-content div:nth-of-type(1) h4"
+);
+bottomContServices.textContent = siteContent["main-content"]["services-h4"];
+
+const para3 = document.querySelector(".bottom-content div:nth-of-type(1) p");
+para3.textContent = siteContent["main-content"]["services-content"];
+
+const bottomContProduct = document.querySelector(
+  ".bottom-content div:nth-of-type(2) h4"
+);
+bottomContProduct.textContent = siteContent["main-content"]["product-h4"];
+
+const para4 = document.querySelector(".bottom-content div:nth-of-type(2) p");
+para4.textContent = siteContent["main-content"]["product-content"];
+
+const bottomContVision = document.querySelector(
+  ".bottom-content div:nth-of-type(3) h4"
+);
+bottomContVision.textContent = siteContent["main-content"]["vision-h4"];
+
+const para5 = document.querySelector(".bottom-content div:nth-of-type(3) p");
+para5.textContent = siteContent["main-content"]["vision-content"];
+
+const contactInfoH4 = document.querySelector(".contact h4");
+contactInfoH4.textContent = siteContent["contact"]["contact-h4"];
+
+const contactInfo1 = document.querySelector(".contact p:nth-of-type(1)");
+contactInfo1.textContent = siteContent["contact"]["address"];
+
+const contactInfo2 = document.querySelector(".contact p:nth-of-type(2)");
+contactInfo2.textContent = siteContent["contact"]["phone"];
+
+const contactInfo3 = document.querySelector(".contact p:nth-of-type(3)");
+contactInfo3.textContent = siteContent["contact"]["email"];
+
+const foot = document.querySelector("footer");
+foot.textContent = siteContent["footer"]["copyright"];
